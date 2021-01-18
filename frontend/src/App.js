736366345d69a086f08cfed54064e4f5515e7ec1
Tsundzukani <trnghondzweni@gmail.com>
2021-01-18 16:48:40 +0200

    Fixing errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'axios'
+import axios from 'axios';
+import { Component } from 'react';
+import  Loading  from './Loading';
+import './table.scss'
 class App extends Component {
   constructor() {
     super();
@@ -8,6 +11,7 @@ class App extends Component {
     this.state = {
         isLoading : true,
         studentData : [],
+        error : false
     }
   }
 
@@ -34,8 +38,67 @@ class App extends Component {
   }
 
   render () {
-    
+    var {
+      isLoading,
+      studentData,
+      error
+    } = this.state;
+
+    if(isLoading){
+      return (
+        <div className="pageContainer">
+             <div className="loginContainer">
+                  <Loading />
+             </div>
+        </div>
+      );
+    }
+    else if (error === true) {
+      return(
+        <div className="pageContainer">
+        <div className="loginContainer">
+             <h1>There was a problem retrieving some of the student data, Please try again ...</h1>
+        </div>
+        </div>
+      )
+    }
+    else{
+    return(
+    <div class="container">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h1>Registree Interview Assessment</h1>
+            <table class="table-latitude">
+            <thead>
+                <tr>
+                    <th>Student ID</th>
+                    <th>Student Name</th>
+                    <th>University</th>
+                    <th>Mark</th>
+                </tr>
+                </thead>
+              <tbody>
+                  {
+                      studentData.map((entry) => (
+                          <tr key={entry}>
+                              <td>{entry.student_id}</td>
+                              <td>{entry.name}</td>
+                              <td>{entry.university}</td>
+                              <td>{entry.mark}</td>
+                              <td/>
+                          </tr>
+                      ))
+                  }
+              </tbody>
+              </table>
+            </div>
+          </div>
+          <div>
+        </div>
+      </div>
+    )
   }
+}
 }
 
 export default App;
