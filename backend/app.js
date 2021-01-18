ca677a32c7933e89894db6e79c914b653f9d380a
Tsundzukani <trnghondzweni@gmail.com>
2021-01-18 16:04:04 +0200

    Backend functionality is complete and working properly

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 
 const student_data_controller = require('./routes/student_data');
 
